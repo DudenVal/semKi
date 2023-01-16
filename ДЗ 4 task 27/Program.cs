854ced6b5881a8d199ca34c2a62cff05cf966cb7
DudenVal <valerapro33@yandex.ru>
2023-01-16 18:53:56 +0300

    В дз добавил решение через методы

--- ДЗ 4 task 27/Program.cs
+++ ДЗ 4 task 27/Program.cs
@@ -4,14 +4,35 @@
 // 82 -> 10
 // 9012 -> 12
 
-System.Console.WriteLine("Enter you number: ");
+// System.Console.WriteLine("Enter you number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int Gnida = 0;                    // Долго маялся с кодом, а решение было простым, нужно было только переменную добавить)
+// int sum = 0;
+// while (numb > 0)
+// {
+//     Gnida = numb % 10;
+//     numb = numb / 10;
+//     sum += Gnida;
+// }
+// Console.Write(sum + " ");
+
+// --------------------------Методом----------------
+
+
+System.Console.WriteLine("Введите переменую A ");
 int numb = Convert.ToInt32(Console.ReadLine());
-int Gnida = 0;                    // Долго маялся с кодом, а решение было простым, нужно было только переменную добавить)
 int sum = 0;
-while (numb > 0)
+int Gnida = 0;
+Console.WriteLine(Sum(numb));
+int Sum(int number)
 {
-    Gnida = numb % 10;
-    numb = numb / 10;
-    sum += Gnida;
+    int N = 1;
+    while (numb > 0)
+    {
+        Gnida = numb % 10;
+        numb = numb / 10;
+        sum += Gnida;
+    }
+    return sum;
 }
-Console.Write(sum + " ");
+

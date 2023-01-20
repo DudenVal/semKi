65e9582a6e2f7aff9945ad17be2a3b0dce6d0a03
DudenVal <valerapro33@yandex.ru>
2023-01-20 16:17:24 +0300

    homework in folder ДЗ C# => ДЗ 6

---
+++ ДЗ C#/ДЗ 6 Task 41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Enter u number: ");
+int NumbA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter u number: ");
+int NumbB = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter u number: ");
+int NumbC = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter u number: ");
+int NumbD = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter u number: ");
+int NumbG = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+if(NumbA > 0) count++;
+if(NumbB > 0) count++;
+if(NumbC > 0) count++;
+if(NumbD > 0) count++;
+if(NumbG > 0) count++;
+
+System.Console.WriteLine($"Количество положительных чисел = " + count);
+
+//Я надеюсь, ничего, если я решил таким легким способом?)
---
+++ ДЗ C#/ДЗ 6 Task 43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Enter b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double tochka = 0;
+double x = 0;
+
+if (b1 == b2 && k1 == k2) System.Console.WriteLine("Прямые совпадают");
+else if (k1 == k2) System.Console.WriteLine("Прямые параллельны");
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    tochka = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+    System.Console.WriteLine($"(" + x + "; " + tochka + ")");
+}
